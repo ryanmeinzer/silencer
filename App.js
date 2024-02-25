c1f4e89287302af1f89f5f3c93a4b56b30cb667c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-02-25 11:34:46 -0800

    refactored with simplification and more adherence to Expo docs, included ToDos

--- App.js
+++ App.js
@@ -1,115 +1,92 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { Audio } from 'expo-av';
 
 export default function App() {
-  const [isMonitoring, setIsMonitoring] = useState(false);
   const [recording, setRecording] = useState(null);
   const [whiteNoiseSound, setWhiteNoiseSound] = useState(null);
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+  const [isMonitoring, setIsMonitoring] = useState(false);
 
-  const recordingSettings = {
-    android: {
-      extension: '.wav',
-      outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_WAV,
-      audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_DEFAULT,
-      sampleRate: 44100,
-      numberOfChannels: 1,
-      bitRate: 128000,
-    },
-    ios: {
-      extension: '.wav',
-      audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_MAX,
-      sampleRate: 44100,
-      numberOfChannels: 1,
-      bitRate: 128000,
-      linearPCMBitDepth: 16,
-      linearPCMIsBigEndian: false,
-      linearPCMIsFloat: false,
-      playsInSilentModeIOS: true,
-    },
-    isMeteringEnabled: true,
-  };
-
-  async function requestMicrophonePermission() {
-    const { status } = await Audio.requestPermissionsAsync();
-    return status === 'granted';
-  }
-
-  async function startRecordingAndMonitoring() {
-    console.log('Starting recording and monitoring...');
-    if (!(await requestMicrophonePermission())) {
-      alert('Microphone permission is required to use this feature.');
-      return;
-    }
-
+  async function startRecording() {
     try {
+      if (permissionResponse.status !== 'granted') {
+        console.log('Requesting permission..');
+        await requestPermission();
+      }
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
-        interruptionModeIOS: 1,
         playsInSilentModeIOS: true,
-        shouldDuckAndroid: true,
-        interruptionModeAndroid: 1,
-        playThroughEarpieceAndroid: false,
+        // ToDo - add UIBackgroundModes to standalong app configuration
+        // https://docs.expo.dev/versions/latest/sdk/audio/#playing-or-recording-audio-in-background
         staysActiveInBackground: true,
       });
 
-      const newRecording = new Audio.Recording();
-      await newRecording.prepareToRecordAsync(recordingSettings);
-      newRecording.setOnRecordingStatusUpdate((status) => {
-        // console.log('Recording status update:', status);
-        if (status.isRecording && status.metering > -30) { 
-          playWhiteNoise();
-          console.log('threshold triggered')
-        }
-      });
-      await newRecording.startAsync();
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
       console.log('Recording started');
-      setRecording(newRecording);
-
+      monitorRecording(recording);
       setIsMonitoring(true);
-    } catch (error) {
-      console.error('Failed to start recording:', error);
+    } catch (err) {
+      console.error('Failed to start recording', err);
     }
+  }
+
+  function monitorRecording(recording) {
+    recording.setOnRecordingStatusUpdate((status) => {
+      // console.log('monitorRecording() -> status:', status);
+      if (status.isRecording && status.metering > -30) { 
+        console.log('threshold triggered')
+        // ToDo - allowsRecordingIOS: false to enhance audio by nearly 2x
+        // stopRecording()
+        playWhiteNoise();
+        // ToDo - re-enable after whiteSound plays, likely need to use playback status
+        // startRecording()
+      }
+    });
   }
 
   // ToDo - allowsRecordingIOS: false to enhance audio by nearly 2x
   async function playWhiteNoise() {
-    console.log('whiteNoiseSound:', whiteNoiseSound);
-    if (!whiteNoiseSound) {
-      const { sound } = await Audio.Sound.createAsync(
-        require('../silencer/white_noise.mp3'),
-        { shouldPlay: true, playsInSilentModeIOS: true }
-      );
-      setWhiteNoiseSound(sound);
-      await sound.setVolumeAsync(0.95); // Set volume to 95%
-      await sound.playAsync(); // Play the sound after it's loaded
-      console.log('inside !whiteNoiseSound')
-    } else {
-      await whiteNoiseSound.playAsync();
-    }
+    const { sound } = await Audio.Sound.createAsync(
+      require('../silencer/white_noise.mp3'),
+      // ToDo - experiment if below is even needed
+      // { shouldPlay: true, playsInSilentModeIOS: true }
+    );
+    setWhiteNoiseSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
   }
 
-  async function stopRecordingAndMonitoring() {
-    if (recording) {
-      await recording.stopAndUnloadAsync();
-      setRecording(null);
-    }
-  
-    if (whiteNoiseSound) {
-      console.log("Stopping and unloading white noise sound");
-      await whiteNoiseSound.stopAsync();
-      await whiteNoiseSound.unloadAsync();
-      setWhiteNoiseSound(null);
-    }
-  
-    setIsMonitoring(false);
-  }  
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    // iOS reroutes audio playback to phone earpiece instead of speaker with allowsRecordingIOS, so disable when playing whiteNoise and once finished
+    await Audio.setAudioModeAsync(
+      {
+        allowsRecordingIOS: false,
+      }
+    );
+    setIsMonitoring(false)
+  }
+
+  useEffect(() => {
+    return whiteNoiseSound
+      ? () => {
+          console.log('Unloading Sound');
+          whiteNoiseSound.unloadAsync();
+        }
+      : undefined;
+  }, [whiteNoiseSound]);
 
   const toggleMonitoring = () => {
     if (isMonitoring) {
-      stopRecordingAndMonitoring();
+      stopRecording();
     } else {
-      startRecordingAndMonitoring();
+      startRecording()
     }
   };
 
